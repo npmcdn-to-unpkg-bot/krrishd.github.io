a96502edd49c69fc6b5495100eb4d292b2367b5e
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-08-21 20:20:44 -0600

    Re-did site a bit better

--- _root/app.js
+++ _root/app.js
@@ -1,9 +1,1 @@
-var app = angular.module('app', []);
 
-function stuffCtrl($scope, $http) {
-	$http.get('http://cors-enabler.herokuapp.com/www.behance.net/v2/users/krishdholakiya/projects?api_key=iaLwUe7zM9AZDIsgsrq5rTRiRGR91vEZ')
-		.success(function(data) {
-			console.log(data);
-			$scope.projects = data.projects.slice(0,6);
-		});
-}
