2373fc6dc8ea84c60594aa6d9d961ccdfa36a098
Krish Dholakiya <krishna.dholakiya@gmail.com>
2015-07-12 15:18:14 -0400

    ......why did i not have google analytics on my personal site of all places

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-65062306-1', 'auto');
+ga('send', 'pageview');
+
 window.onload = function() {
   var api = 'www.behance.net/v2/users/krishdholakiya/projects?api_key=iaLwUe7zM9AZDIsgsrq5rTRiRGR91vEZ';
   http.GET('http://cors-enabler.herokuapp.com/' + api, function(data) {
