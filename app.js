df99211755a99f48d7a6ea9f9c2eb2d179a19b84
Krish Dholakiya <krishna.dholakiya@gmail.com>
2015-07-12 15:33:22 -0400

    add loading indicator for work, also add basic stateful structure

--- app.js
+++ app.js
@@ -3,11 +3,21 @@
 ga('create', 'UA-65062306-1', 'auto');
 ga('send', 'pageview');
 
+var state = {
+	loading: true
+};
+
 window.onload = function() {
+  Object.observe(state, function() {
+	document.querySelector('.loading')
+	  .setAttribute('class', 'loading ' + state.loading);
+  });
   var api = 'www.behance.net/v2/users/krishdholakiya/projects?api_key=iaLwUe7zM9AZDIsgsrq5rTRiRGR91vEZ';
+  state.loading = true;
   http.GET('http://cors-enabler.herokuapp.com/' + api, function(data) {
     var res = JSON.parse(data);
     if (res.http_code === 200) {
+      state.loading = false;
       res.projects.forEach(function(project) {
         var item = document.createElement('li');
         item.innerHTML = '<a title="' + project.name + '" href="' + project.url + '" target="_blank"><img class="project" src="' + project.covers[230] + '"></a>';
